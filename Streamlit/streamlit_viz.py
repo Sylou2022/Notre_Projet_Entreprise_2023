c0444dd0b202f1f4626e7d1c27f1cf24da4679b1
Diouf <sylvaindiouf18@gmail.com>
2023-06-08 00:50:06 +0200

    une retouche

--- Streamlit/streamlit_viz.py
+++ Streamlit/streamlit_viz.py
@@ -12,6 +12,8 @@ from menu.etoile_periode import etoile_periode
 from menu.tab_data import tab_data
 from menu.pourcentage_etoiles import prc_etoile
 from menu.accueil import accueil
+import plotly.graph_objects as go
+from matplotlib import pyplot as plt
 
 # def main():
 
@@ -179,7 +181,10 @@ def main():
         selected_option = st.sidebar.selectbox("", ["Accueil", "Carte des agences", "Pourcentages des Etoiles" ,"Tableau de données", "Nombre d'avis positifs par ville /année", "Qualité de services par Ville","Taux des avis par ville", "Les Scores par ville","Entrainement du modèle NLP"])
 
         if selected_option == "Accueil":
-            accueil()
+   
+            
+            st.markdown("<h1 style='color: #000000;'>Tableau de Bord</h1>", unsafe_allow_html=True)
+            st.markdown("<h2 style='color: #000000;'>Bienvenue dans Projet Entreprise</h2>", unsafe_allow_html=True)
             
         elif selected_option == "Carte des agences":
             
@@ -187,25 +192,289 @@ def main():
             webbrowser.open('carte_pole_emploi.html')
         
         elif selected_option == "Pourcentages des Etoiles":
-            prc_etoile()
-        
+            st.markdown("<h2 style='color: #000000;'>Somme des Etoiles en %</h2>", unsafe_allow_html=True)
+            st.markdown("La répartition en pourcentage des différentes catégories d'étoiles. Les résultats montrent que 1 étoile représente 56,5% dépassant la moyenne. Ce graphique met en évidence la prédominance de la catégorie 1 étoile. ", unsafe_allow_html=True)
+            Labels = ["\u2605", '\u2605\u2605', '\u2605\u2605\u2605', '\u2605\u2605\u2605\u2605', '\u2605\u2605\u2605\u2605\u2605']
+            sizes = [1305, 104, 131, 154, 616]
+            color = ['#ff0000', '#ffa700', '#fff400', '#a3ff00', '#2cba00']
+
+            fig1, ax1 = plt.subplots()
+            ax1.pie(sizes, colors=color, labels=Labels, autopct='%1.1f%%', startangle=90)
+
+            # Cercle de dessin
+            centre_circle = plt.Circle((0, 0), 0.70, fc='white')
+            fig = plt.gcf()
+            fig.gca().add_artist(centre_circle)
+
+            # Le rapport hauteur / largeur égal garantit que la tarte est dessinée sous forme de cercle
+            ax1.axis('equal')
+            plt.tight_layout()
+            st.pyplot(fig)  
+
         elif selected_option == "Tableau de données":
-            tab_data()
+            st.markdown("<h2 style='color: #000000;'>Tableau de données</h2>", unsafe_allow_html=True)
+            st.markdown("Le tableau de données obtenu après le scraping contient plus de 2300 lignes", unsafe_allow_html=True)
+            data = pd.read_csv('../Data/csv/Île-de-France_POLE_EMPLOI_copie.csv', nrows=2337, sep=';')
+            data['Date'] = pd.to_datetime(data['Date'])
+            st.write(data)
             
         elif selected_option == "Nombre d'avis positifs par ville /année":
-            avis_positif_annee()
+            
+             # Charger les données depuis le fichier CSV
+            df = pd.read_csv('../Data/csv/avi.csv', sep=';')
+
+            # Extraire l'année à partir de la colonne "Date de publication"
+            df['Year'] = pd.to_datetime(df['Date de publication'], format='%d/%m/%Y').dt.strftime('%Y')
+
+            # Récupérer toutes les années du CSV
+            annees = df['Year'].unique()
+
+            st.markdown("<h6 >Visualisation de l'évolution des avis positifs au fil des années et comparaison des différentes villes. L’analyse sur la satisfaction des utilisateurs dans chaque ville.Les variations temporelles et géographiques des avis positifs.</h6>", unsafe_allow_html=True)
+            # Sélection de la ville via le dropdown
+            selected_ville = st.selectbox('Sélectionnez une ville', df['Ville'].unique())
+
+            # Filtrer les données pour la ville sélectionnée
+            positifs_par_ville = df[df['Ville'] == selected_ville]
+            positifs_par_ville = positifs_par_ville[positifs_par_ville['Type'] == 'Positif'].groupby(['Year']).size().reset_index(name='Count')
+
+            # Créer le graphique en courbe
+            fig = px.line(positifs_par_ville, x='Year', y='Count')
+            
+
+            # Mise en forme du layout du graphique
+            fig.update_layout(title=f'Représentation du nombre d\'avis positifs par année pour la ville : {selected_ville}',
+
+
+            yaxis_title='Nombre d\'avis positifs')
+            fig.update_xaxes(title_text='Année',dtick=1)
+            
+            fig.update_yaxes(title_text='Nombre d\'avis positifs',dtick=1)
+
+            # Afficher le graphique en courbe
+            st.plotly_chart(fig)
             
         elif selected_option == "Qualité de services par Ville":
-            qual_serv_ville()
+            # Charger les données depuis le fichier CSV
+            df = pd.read_csv('../Data/csv/taux_ville.csv', sep=';')
+
+            st.markdown("<h6 >Représentation de la qualité des services par villes en fonction des avis. Perception des utilisateurs et l’identification des villes ayant les meilleurs et les moins bons résultats en termes de satisfaction des services. Analyse comparative et mise en évidence des variations entre les différentes localités.</h6>", unsafe_allow_html=True)
+
+            # Sélection de la colonne "Ville" via le dropdown
+            selected_ville = st.selectbox('Sélectionnez une ville', df['Ville'].unique())
+
+            # Filtrer les données pour la ville sélectionnée
+            ville_data = df[df['Ville'] == selected_ville]
+
+            # Récupérer les nombres d'avis positifs, négatifs et neutres
+            avis_positifs = ville_data["Nombre d’avis Positif"].iloc[0]
+            avis_negatifs = ville_data["Nombre d’avis négatif"].iloc[0]
+            avis_neutres = ville_data["Nombre d’avis Neutre"].iloc[0]
+
+            # Créer les données pour le graphique en barres
+            categories = ['Avis positifs', 'Avis négatifs', 'Avis neutres']
+            values = [avis_positifs, avis_negatifs, avis_neutres]
+            colors = ['green', 'red', 'yellow']
+
+            # Créer le graphique en barres
+            fig = go.Figure(data=[go.Bar(x=categories, y=values, marker=dict(color=colors))])
+
+            # Mise en forme du layout du graphique en barres
+            fig.update_layout(title='Qualité de service pour la ville : ' + selected_ville,
+                xaxis_title='Types d\'avis',
+                yaxis_title='Nombre d\'avis')
+
+            # Afficher le graphique en barres
+            st.plotly_chart(fig)
             
         elif selected_option == "Taux des avis par ville":
-            avis_prc_ville()
+        
+                    # Charger les données des scores des villes d'Île-de-France
+            datafr = pd.read_csv('../Data/csv/taux_ville.csv', sep=';')
+
+            # Créer une liste des options de sélection pour le dropdown
+            villes = datafr['Ville'].unique()
+            
+            st.markdown("<h6>La répartition des avis par ville et l’observation des préférences et opinions des utilisateurs. La visualisation claire des proportions relatives des avis positifs, négatifs et neutres, facilitant ainsi la détection de tendances et de disparités entre les villes en termes de satisfaction des services.</h6>", unsafe_allow_html=True)
+
+            # Sélection de la ville via le dropdown
+            selected_ville = st.selectbox('Sélectionnez une ville', villes)
+
+            # Filtrer les données pour la ville sélectionnée
+            ville_data = datafr[datafr['Ville'] == selected_ville]
+
+            # Récupérer le nombre d'avis négatifs, positifs et neutres
+            avis_negatifs = ville_data["Nombre d’avis négatif"].iloc[0]
+            avis_positifs = ville_data["Nombre d’avis Positif"].iloc[0]
+            avis_neutres = ville_data["Nombre d’avis Neutre"].iloc[0]
+
+            # Calculer le pourcentage des avis négatifs, positifs et neutres
+            total_avis = avis_negatifs + avis_positifs + avis_neutres
+            pourcentage_negatifs = (avis_negatifs / total_avis) * 100
+            pourcentage_positifs = (avis_positifs / total_avis) * 100
+            pourcentage_neutres = (avis_neutres / total_avis) * 100
+
+            # Créer une figure en utilisant le graphique semi-circulaire de Plotly pour les avis
+            fig_avis = go.Figure()
+
+            # Ajouter une trace semi-circulaire pour les avis négatifs
+            fig_avis.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=pourcentage_negatifs,
+                domain={'x': [0, 0.3], 'y': [0, 1]},
+                title={'text': '% Avis négatifs'},
+                gauge={'axis': {'range': [0, 100]},
+                    'bar': {'color': 'red'},
+                    'steps': [{'range': [0, 50], 'color': 'red'}],
+                    'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': pourcentage_negatifs}}))
+
+            # Ajouter une trace semi-circulaire pour les avis positifs
+            fig_avis.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=pourcentage_positifs,
+                domain={'x': [0.35, 0.65], 'y': [0, 1]},
+                title={'text': '% Avis positifs'},
+                gauge={'axis': {'range': [0, 100]},
+                    'bar': {'color': 'green'},
+                    'steps': [{'range': [0, 50], 'color': 'green'}],
+                    'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': pourcentage_positifs}}))
+
+
+            # Ajouter une trace semi-circulaire pour les avis neutres
+            fig_avis.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=pourcentage_neutres,
+                domain={'x': [0.7, 1], 'y': [0, 1]},
+                title={'text': ' % Avis neutres'},
+                gauge={'axis': {'range': [0, 100]},
+                    'bar': {'color': 'blue'},
+                    'steps': [{'range': [0, 50], 'color': 'blue'}],
+                    'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': pourcentage_neutres}}))
+
+            # Créer une figure en utilisant le graphique semi-circulaire de Plotly pour la performance totale
+            fig_performance = go.Figure()
+
+            # Calculer la performance totale en pourcentage
+            performance_totale = (avis_positifs / total_avis) * 100
+
+            # Ajouter une trace semi-circulaire pour la performance totale
+            fig_performance.add_trace(go.Indicator(
+                mode="gauge+number",
+                value=performance_totale,
+                domain={'x': [0, 1], 'y': [0, 1]},
+                title={'text': 'Etude Comparative - Performance totale'},
+                gauge={'axis': {'range': [0, 100]},
+                    'bar': {'color': 'purple'},
+                    'steps': [{'range': [0, 100], 'color': 'purple'}],
+                    'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': performance_totale}}))
+
+            # Mise en forme du layout des graphiques
+            fig_avis.update_layout(height=400, width=600, margin=dict(l=20, r=20, t=30, b=20))
+            # fig_performance.update_layout(height=400, width=400, margin=dict(l=20, r=20, t=30, b=20))
+
+            # Afficher les graphiques
+            st.plotly_chart(fig_avis, use_container_width=True)
+            # st.plotly_chart(fig_performance, use_container_width=True)
             
         elif selected_option == "Les Scores par ville":
-            score_ville()
+            datafr = pd.read_csv('../Data/csv/taux_ville.csv', sep=';')
+            # Créer une liste des options de sélection pour le dropdown
+            villes = datafr['Ville'].unique()
+
+
+            st.markdown("<h6>Représentation visuelle des quantités d'avis positifs, négatifs et neutres dans chaque ville à travers des données brutes. Etablir une étude approfondie pour identifier les tendances et les disparités entres les villes en termes de satisfaction de services.</h6>", unsafe_allow_html=True)
+
+            # Sélection de la ville via le dropdown
+            selected_ville = st.selectbox('Sélectionnez une ville', villes)
+
+            # Filtrer les données pour la ville sélectionnée
+            ville_data = datafr[datafr['Ville'] == selected_ville]
+
+            # Récupérer le nombre d'avis négatifs, positifs et neutres
+            avis_positifs = ville_data["Nombre d’avis Positif"].iloc[0]
+            avis_negatifs = ville_data["Nombre d’avis négatif"].iloc[0]        
+            avis_neutres = ville_data["Nombre d’avis Neutre"].iloc[0]
+
+
+            score_positifs = avis_positifs
+            score_negatifs = avis_negatifs       
+            score_neutres = avis_neutres
+
+            # Créer une figure en utilisant le graphique semi-circulaire de Plotly pour les avis
+            fig_avis = go.Figure()
+
+
+            # Ajouter une trace semi-circulaire pour les avis positifs
+            fig_avis.add_trace(go.Indicator(
+            mode="gauge+number",
+            value=score_positifs,
+            domain={'x': [0.35, 0.65], 'y': [0, 1]},
+            title={'text': 'Avis positifs'},
+            gauge={'axis': {'range': [0, 100]},
+            'bar': {'color': 'green'},
+            'steps': [{'range': [0, 50], 'color': 'green'}],
+            'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': score_positifs}}))
+
+
+            # Ajouter une trace semi-circulaire pour les avis négatifs
+            fig_avis.add_trace(go.Indicator(
+            mode="gauge+number",
+            value=score_negatifs,
+            domain={'x': [0, 0.3], 'y': [0, 1]},
+            title={'text': 'Avis négatifs'},
+            gauge={'axis': {'range': [0, 100]},
+            'bar': {'color': 'red'},
+            'steps': [{'range': [0, 50], 'color': 'red'}],
+            'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': score_negatifs}}))
+
+
+            # Ajouter une trace semi-circulaire pour les avis neutres
+            fig_avis.add_trace(go.Indicator(
+            mode="gauge+number",
+            value=score_neutres,
+            domain={'x': [0.7, 1], 'y': [0, 1]},
+            title={'text': ' Avis neutres'},
+            gauge={'axis': {'range': [0, 100]},
+            'bar': {'color': 'blue'},
+            'steps': [{'range': [0, 50], 'color': 'yellow'}],
+            'threshold': {'line': {'color': 'black', 'width': 4}, 'thickness': 0.75, 'value': score_neutres}}))
+
+
+            # Mise en forme du layout des graphiques
+            fig_avis.update_layout(height=400, width=600, margin=dict(l=20, r=20, t=30, b=20))
+
+
+            # Afficher les graphiques
+            st.plotly_chart(fig_avis, use_container_width=True)
         
         elif selected_option == "Nombre d'étoiles par période":
-            etoile_periode()
+            dfm = pd.read_csv('../Data/csv/Île-de-France_POLE_EMPLOI_copie.csv', sep=';')
+            st.subheader('Nombre Etoiles par période')
+
+            # Convertir les valeurs float en str dans la colonne "Date"
+            dfm['Date'] = dfm['Date'].apply(lambda x: str(x) if isinstance(x, float) else x)
+
+            # Exclure les valeurs 'nan' de la colonne "Date"
+            dfm = dfm.dropna(subset=['Date'])
+
+            # Appliquer la fonction strptime() en gérant les exceptions pour les valeurs non valides
+            dfm['ParsedDate'] = dfm['Date'].apply(lambda x: pd.NaT if x == 'nan' else datetime.datetime.strptime(x, "%d/%m/%Y") if isinstance(x, str) else pd.NaT)
+
+            # Supprimer les lignes avec des valeurs non valides
+            dfm = dfm.dropna(subset=['ParsedDate'])
+
+            # Regrouper par année et calculer la somme des étoiles
+            etoiles_sum = dfm.groupby(dfm['ParsedDate'].dt.year)['Etoile'].sum()
+
+            periodes = etoiles_sum.index
+            etoiles = etoiles_sum.values
+
+            fig = plt.figure()
+            plt.plot(periodes, etoiles, label='Nombre Etoiles')
+            plt.xlabel('Années')
+            plt.ylabel('Somme des étoiles')
+            plt.legend()
+            plt.xticks(periodes, labels=[str(year) for year in periodes])  # Afficher les années au format "2023"
+            st.pyplot(fig)
 
         elif selected_option == "Total avis en % par ville":
             avis_prc_ville()          
